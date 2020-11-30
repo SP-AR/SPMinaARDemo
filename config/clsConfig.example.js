959ce93a1d462621bfd2e4b5649a44ab5c449676
Damo <daiguimao@sightp.com>
2020-11-30 15:21:50 +0800

    crs/cls example注释

--- config/clsConfig.example.js
+++ config/clsConfig.example.js
@@ -1,9 +1,9 @@
 module.exports = {
   clsConfig: {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    apiSecret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    ocKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    ocSecret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    schemaId: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // EasyAR开发中心 - API KEY - API Key
+    apiSecret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // EasyAR开发中心 - API KEY - API Secret
+    ocKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // AR运营中心 - 访问秘钥 - Key
+    ocSecret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // AR运营中心 - 访问秘钥 - Secret
+    schemaId: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", // AR运营中心 - 选择AR启动方案 - ID
   },
 }
--- config/crsConfig.example.js
+++ config/crsConfig.example.js
@@ -6,10 +6,11 @@ module.exports = {
   sceneFileName: '935237.json',
   // 用于crs识别，若不使用crs识别则无需添加该属性
   crsConfig: {
-    accessKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    accessSecret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    clientHost: 'https://xxx-xxx.xxxxxx.xxx:xxxx'
+    accessKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // EasyAR 开发中心-云识别管理-秘钥-小程序 AR 使用 - Cloud Key
+    accessSecret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // EasyAR 开发中心-云识别管理-秘钥-小程序 AR 使用 - Cloud Secret
+    clientHost: 'https://xxx-xxx.xxxxxx.xxx:xxxx', // EasyAR 开发中心-云识别管理-秘钥-小程序 AR 使用 - Cloud URLs
   },
   // 云识别管理中的识别图ID，开启扫描后会持续请求服务，直到扫描结果在 targetIds 中
-  targetIds: ['xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'],
+  targetIds: ['xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'],// EasyAR 开发中心-云识别管理-识别图-识别图 ID
+  license: '', // 许可证 内部小程序-填了会校验license
 }
