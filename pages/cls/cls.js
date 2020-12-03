1d39a5e7c9a0c32722e3c10373dcbf0d50967968
Damo <daiguimao@sightp.com>
2020-12-03 16:08:50 +0800

    fix clsconfig

--- pages/cls/cls.js
+++ pages/cls/cls.js
@@ -1,5 +1,5 @@
+import clsConfig from '../../config/clsConfig';
 const sysInfo = wx.getSystemInfoSync();
-
 Page({
   data: {
     wxapi: wx,
@@ -15,15 +15,7 @@ Page({
     showScan: true,
   },
   onLoad: function(options) {
-    this.setData({
-      clsConfig: { //devdev账号 园区新V2-0907
-        apiKey: "118b4dacf7af90c67e790cb87750266a",
-        apiSecret: "6059dd99ad6341e892015ed52332b1f79fc54d325af4478bb25668a247503ac0",
-        ocKey: "540738eaf9a2829533f0691910ce25c7",
-        ocSecret: "d4dd2b0df609db28281d1febec9968144bce01d21d344fe17484bb1484da8c18",
-        schemaId: "b086fbce-dcea-465c-ac97-34fa22e6e87d",
-      },
-    });
+    this.setData(clsConfig);
   },
 
   pause() {
